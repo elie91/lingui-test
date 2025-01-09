205c472089a1d317b9cc105af148f262a1f197f3
Elie Bismuth <eliebismuth91@gmail.com>
2025-01-09 10:25:45 +0100

    up

--- src/app/locale-provider.tsx
+++ src/app/locale-provider.tsx
@@ -15,7 +15,7 @@ i18n.load({
   es,
   de,
 });
-i18n.activate("fr");
+i18n.activate("en");
 
 export const LocaleProvider: React.FC<PropsWithChildren> = ({ children }) => {
   return <I18nProvider i18n={i18n}>{children}</I18nProvider>;
