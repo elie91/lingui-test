c38cd1943759704e6ec08023c65650afdc38d000
Elie Bismuth <eliebismuth91@gmail.com>
2025-01-09 09:34:19 +0100

    add another page

---
+++ src/app/demo/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { Trans } from "@lingui/react/macro";
+import Image from "next/image";
+
+export default function Demo() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li className="mb-2">
+            <Trans>Get started by editing</Trans>{" "}
+            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+              src/app/page.tsx
+            </code>
+            .
+          </li>
+          <li>
+            <Trans>Save and see your changes instantly.</Trans>
+          </li>
+        </ol>
+      </main>
+    </div>
+  );
+}
